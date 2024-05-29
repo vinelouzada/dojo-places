2cc048d9d8d63ec29afcd5549b40d3420f9e8d2c
Gabriel Ronei <gabriheron@hotmail.com>
2024-05-29 17:03:50 -0300

    chore(project): atualizando versão do projeto

--- src/main/java/br/com/alura/dojoplaces/DojoPlacesApplication.java
+++ src/main/java/br/com/alura/dojoplaces/DojoPlacesApplication.java
@@ -2,12 +2,19 @@ package br.com.alura.dojoplaces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class DojoPlacesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DojoPlacesApplication.class, args);
-	}
+	@GetMapping("/oi")
+    public String oi() {
+        return "/oi";
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(DojoPlacesApplication.class, args);
+    }
 }
